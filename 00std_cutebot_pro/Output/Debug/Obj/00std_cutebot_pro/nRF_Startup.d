c0f2804a8a771994f1c2a0a27e115749a019f18a
Gustavo Barreto <gbarreto@estudante.ufscar.br>
2026-05-07 02:14:34 +0200

    Project Cutebot

--- 00std_cutebot_pro/Output/Debug/Obj/00std_cutebot_pro/nRF_Startup.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/00std_cutebot_pro/nRF_Startup.o: \
- /home/mgustav/Documents/ENSTA/IoT-Classes/microbit/common/system/nRF/Source/nRF_Startup.s
--- 00std_cutebot_pro/Output/Debug/Obj/00std_cutebot_pro/nrf52833_Vectors.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/00std_cutebot_pro/nrf52833_Vectors.o: \
- /home/mgustav/Documents/ENSTA/IoT-Classes/microbit/common/system/nRF/Source/nrf52833_Vectors.s
